336199c69fa456c9d87286e7ca33926844f62568
Volodymyr <63854674+efplay@users.noreply.github.com>
2026-03-31 21:10:15 +0200

    31-03 add html for main page

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import greeting
+from .views import *
 
 urlpatterns = [
-    path('greeting/', greeting),
+    path('', notes_main, name='notes_main'),
+    
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,13 @@
+from urllib import request
+
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView
+from django import forms
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponseRedirect
 
 
-
-def greeting(request):
-    html = "<h1>Hello from Notes app.</h1>"
-    return HttpResponse(html)
+def notes_main(request):
+    return render(request, 'notes.html')
+    
